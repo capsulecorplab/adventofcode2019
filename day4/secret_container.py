8eb35989cdca3b5f2f9c66dbc05b485964f43d6a
Sean Marquez <capsulecorplab@gmail.com>
2019-12-23 11:11:27 -0800

    add day4 part2
     - refactor _meets_criteria() & no_of_pw_combinations() for part2

--- day4/secret_container.py
+++ day4/secret_container.py
@@ -14,6 +14,8 @@
 # >>> pc = PasswordCracker()
 # >>> pc.no_of_pw_combinations(382345, 843167)
 # 460
+# >>> pc.no_of_pw_combinations(382345, 843167, part=2)
+# 290
 # >>>
 ################################################################################
 
@@ -22,26 +24,47 @@ import re
 
 class PasswordCracker:
     @classmethod
-    def no_of_pw_combinations(cls, lower_bound: int, upper_bound: int) -> int:
+    def no_of_pw_combinations(
+        cls, lower_bound: int, upper_bound: int, part: int = 1
+    ) -> int:
         pw_combinations = []
         for pw in range(lower_bound, upper_bound + 1):
-            if cls._meets_criteria(pw):
+            if cls._meets_criteria(pw, part):
                 pw_combinations.append(pw)
         return len(pw_combinations)
 
     @classmethod
-    def _meets_criteria(cls, pw: int) -> bool:
-        return len(str(pw)) > 0 and cls._isincreasing(pw) and cls._hasdouble(pw)
+    def _meets_criteria(cls, pw: int, part: int = 1) -> bool:
+        if part == 1:
+            return len(str(pw)) > 0 and cls._isincreasing(pw) and cls._hasdouble(pw)
+        elif part == 2:
+            return (
+                len(str(pw)) > 0
+                and cls._isincreasing(pw)
+                and cls._hasdouble_not_of_larger_group(pw)
+            )
 
     @staticmethod
     def _isincreasing(pw: int) -> bool:
         pw_str = str(pw)
         i_0 = 0
-        for i in pw_str:
-            if i_0 > int(i):
+        for e in pw_str:
+            if i_0 > int(e):
                 return False
-            i_0 = int(i)
+            i_0 = int(e)
         return True
+
+    @staticmethod
+    def _hasdouble_not_of_larger_group(pw: int) -> bool:
+        pw_str = str(pw)
+        two_or_more = []
+        for _ in range(10):
+            regex = re.findall(str(_), pw_str)
+            if len(regex) >= 2:
+                two_or_more.append(len(regex))
+        if 2 in two_or_more:
+            return True
+        return False
 
     @staticmethod
     def _hasdouble(pw: int) -> bool:
@@ -59,3 +82,4 @@ if __name__ == "__main__":
 
     pc = PasswordCracker()
     print(pc.no_of_pw_combinations(lower_bound, upper_bound))
+    print(pc.no_of_pw_combinations(lower_bound, upper_bound, 2))
--- day4/test_secret_container.py
+++ day4/test_secret_container.py
@@ -18,3 +18,18 @@ class Test1:
 
     def test_no_of_pw_combinations(self, pc):
         assert pc.no_of_pw_combinations(382345, 843167) == 460
+
+
+class Test2:
+    @pytest.fixture
+    def pc(self):
+        return PasswordCracker()
+
+    def test_meets_criteria(self, pc):
+        assert pc._meets_criteria(112233, part=2) == True
+        assert pc._meets_criteria(123444, part=2) == False
+        assert pc._meets_criteria(111122, part=2) == True
+        assert pc._meets_criteria(111222, part=2) == False
+
+    def test_no_of_pw_combinations(self, pc):
+        assert pc.no_of_pw_combinations(382345, 843167, part=2) == 290
